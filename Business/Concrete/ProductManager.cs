60328801be96596b3429ca81774fea330ca9397b
Berke Can Bal <berkebaltr@hotmail.com>
2021-02-17 18:09:47 +0300

    17 şubat düzenleme 18:09

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -36,7 +36,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
@@ -60,6 +60,10 @@ namespace Business.Concrete
 
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
+            if (DateTime.Now.Hour==23)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
             return new SuccessDataResult<List<ProductDetailDto>> (_productDal.GetProductDetails());
         }
     }
